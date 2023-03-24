37f29aaf8063641ff54dd084c28d08fc93edda27
Parth Dhruve <parthdhruve@gmail.com>
2023-03-23 20:02:30 -0400

    Moved to a function for setup.sh and added some enviroment variables.

--- setup.sh
+++ setup.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-{ echo -n "source " & echo `readlink -f ./bash/.bashrc.mine`; } >> ~/.bash_profile
+
+append_source () {
+{ echo -n "source " & echo `readlink -f $1`; } >> "$2"
+}
+
+append_source ./bash/.bashrc.mine ~/.bash_profile
+append_source ./vim/.vimrc.mine ~/.vimrc
